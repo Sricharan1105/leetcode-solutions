e5be4f668052004864a3c0ff122cd2a9d6f15e98
S P SRICHARAN <ss8968@srmist.edu.in>
2026-05-29 21:58:01 +0530

    Implement minElement function to find minimum digit sum

---
+++ Easy/Testcase Testcase Test Result Test Result Accep3300_Minimum_Element_After_Replacement_With_Digit_Sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        
+        min_sum = float('inf')
+
+        for num in nums:
+
+            digit_sum = 0
+
+            while num > 0:
+                digit_sum += num % 10
+                num //= 10
+
+            min_sum = min(min_sum, digit_sum)
+
+        return min_sum
