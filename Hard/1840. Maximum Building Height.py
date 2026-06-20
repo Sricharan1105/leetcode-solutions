40ea03e3b7faa1b2d9bac3eeb7c419f34690d5c8
S P SRICHARAN <ss8968@srmist.edu.in>
2026-06-20 15:29:30 +0530

    Add maxBuilding function to calculate heights
    Implement maxBuilding function to calculate the maximum building height given restrictions.

---
+++ Hard/1840. Maximum Building Height.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
+        restrictions.append([1, 0])
+        if restrictions[-1][0] != n:
+            restrictions.append([n, n - 1])
+
+        restrictions.sort()
+
+        # Left to right
+        for i in range(1, len(restrictions)):
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i - 1][1] + restrictions[i][0] - restrictions[i - 1][0]
+            )
+
+        # Right to left
+        for i in range(len(restrictions) - 2, -1, -1):
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i + 1][1] + restrictions[i + 1][0] - restrictions[i][0]
+            )
+
+        ans = 0
+
+        for i in range(1, len(restrictions)):
+            x1, h1 = restrictions[i - 1]
+            x2, h2 = restrictions[i]
+            d = x2 - x1
+            ans = max(ans, (h1 + h2 + d) // 2)
+
+        return ans
